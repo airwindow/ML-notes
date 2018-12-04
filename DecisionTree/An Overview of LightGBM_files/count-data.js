00ab32e0e22981d4e1b7e340a5bbc84d28ddaa8e
JINGWEI YANG <jingwei@us.ibm.com>
2018-12-04 10:38:27 -0500

    add more notes

---
+++ DecisionTree/An Overview of LightGBM_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"https:\/\/www.avanwyk.com\/an-overview-of-lightgbm\/","comments":0}]});
+}
